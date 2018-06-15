fa3e7a1733b2dbc1b97631dc02b1ac7f3e32238a
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-06-14 22:10:37 -0500

    Add validations

--- js/app.js
+++ js/app.js
@@ -7,7 +7,12 @@ const btnReset = document.querySelector('#resetBtn');
 
 
 // ESCUCHADOR DE EVENTOS
+// inicio de la aplicación y desabilitar botón
 document.addEventListener('DOMContentLoaded', inicioApp);
+// campos del formulario
+email.addEventListener('blur', validarCampo);
+asunto.addEventListener('blur', validarCampo);
+mensaje.addEventListener('blur', validarCampo);
 
 
 
@@ -16,4 +21,44 @@ document.addEventListener('DOMContentLoaded', inicioApp);
 function inicioApp() {
     // desabilitar el botón de envíar
     btnEnviar.disabled = true;
+}
+
+// Valida que el campo tenga algo escrito
+function validarCampo() {
+    // Se valida la longitud del texto y que no este vacio
+    validarLongitud(this);
+
+    let errores = document.querySelectorAll('.error');
+
+    // validar unicamente el email
+    if(this.type === 'email') {
+        validarEmail(this);
+    }
+
+    if(email.value !== '' && asunto.value !== '' && mensaje.value !== '') {
+        if(errores.length === 0) {
+            btnEnviar.disabled = false;
+        }
+    }
+}
+
+function validarLongitud(campo) {
+    if(campo.value.length > 0) {
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    } else {
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    }
+}
+
+function validarEmail(campo) {
+    const mensaje = campo.value;
+    if(mensaje.indexOf('@') !== -1) {
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    } else {
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    }
 }
